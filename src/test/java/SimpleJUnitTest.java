79de5190622990186ad2038127258571ed68d81a
Lilyaboni <lilyafugina@gmail.com>
2026-04-03 14:41:12 +0500

    added JUnit 5

---
+++ src/test/java/SimpleJUnitTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SimpleJUnitTest {
+    int result;
+
+    @BeforeEach
+    void beforeEach () {
+        result = getResult();
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("###   firstTest()");
+        Assertions.assertTrue(result > 2);
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("###   secondTest()");
+        Assertions.assertTrue(result > 2);
+    }
+
+    @Test
+    void thirdTest() {
+        System.out.println("###   thirdTest()");
+        Assertions.assertTrue(result > 2);
+    }
+
+    private int getResult() {
+        return 3;
+    }
+}
